d89447779d21e14614d26aaaf884121f3b1154d1
yohan-oc <129070827+yohan-oc@users.noreply.github.com>
2024-12-11 00:17:20 +0000

    day 02 part 2 failed

--- src/Aoc2024/Aoc2024/Day09/Solution.cs
+++ src/Aoc2024/Aoc2024/Day09/Solution.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Data;
 using System.Data.Common;
 using System.Diagnostics.CodeAnalysis;
 using System.Drawing;
@@ -12,7 +13,7 @@ namespace Aoc2024.Day09
     {
         public static void Part01()
         {
-            Console.WriteLine("**** Day 09 ****");
+            Console.WriteLine("**** Day 09 - Part 01 ****");
             
             string diskMap = File.ReadAllLines("Day09/input.txt")[0];
 
@@ -79,6 +80,8 @@ namespace Aoc2024.Day09
             //Console.WriteLine("Individual blocks: " + newDiskMap);
 
             int spaceBlockSize = blocks.Count(c => c.Value.Type == BlockType.Space);
+            
+            //Console.WriteLine("Total blocks: " + blocks.Count);
 
             for (int blockCount = 0; blockCount < spaceBlockSize; blockCount++)
             {
@@ -91,7 +94,7 @@ namespace Aoc2024.Day09
                 
                 //PrintBlocks(blocks);
                 
-                Console.WriteLine("Rearranging: " + blockCount);
+                //Console.WriteLine("Rearranging: " + blockCount);
             }
 
             
@@ -118,16 +121,6 @@ namespace Aoc2024.Day09
             Console.WriteLine("check sum: " + total);
         }
         
-        public static void Part02()
-        {
-            string[] lines = File.ReadAllLines("Day09/input.txt");
-
-            Int64 rowLengthIndex = lines.Length - 1;
-            Int64 colLengthIndex = lines[0].Length - 1;
-            
-            Console.WriteLine("Day 09: ");
-        }
-
         static void PrintBlocks(Dictionary<int, Block> blocks)
         {
             string newDiskMap = "";
@@ -135,20 +128,220 @@ namespace Aoc2024.Day09
             {
                 // for (int blockCount = 0; blockCount < block.Value.Size; blockCount++)
                 // {
-                    if (block.Value.Type == BlockType.File)
-                    {
-                        Console.Write(block.Value.Id);
-                        newDiskMap = newDiskMap + "x" + block.Value.Id;
-                    }
-                    else
-                    {
-                        Console.Write(".");
-                        newDiskMap = newDiskMap + ".";
-                    }
+                if (block.Value.Type == BlockType.File)
+                {
+                    Console.Write(block.Value.Id);
+                    newDiskMap = newDiskMap + "x" + block.Value.Id;
+                }
+                else
+                {
+                    Console.Write(".");
+                    newDiskMap = newDiskMap + ".";
+                }
                 //}
             }
             Console.WriteLine();
 
+        }
+        
+        public static void Part02()
+        {
+            Console.WriteLine("**** Day 09 - Part 02 ****");
+            
+            string diskMap = File.ReadAllLines("Day09/input.txt")[0];
+
+            var blocks = new Dictionary<int, Block>();
+            
+            int blockIndex = 0;
+            int spaceIndex = 0;
+            int indexCount = 0;
+            
+            for (int index = 0; index < diskMap.Length; index++)
+            {
+                var size = int.Parse(diskMap[index].ToString());
+                if (index % 2 == 0)
+                {
+                    blocks.Add(indexCount,
+                        new Block
+                        {
+                            Type = BlockType.File, Id = blockIndex, Size = size
+                        });
+                    indexCount = indexCount + 10;
+                    blockIndex++;
+                }
+                else
+                {
+                    if (size != 0)
+                    {
+                        blocks.Add(indexCount,
+                            new Block
+                            {
+                                Type = BlockType.Space, Id = spaceIndex, Size = size
+                            });
+                        indexCount = indexCount + 10;
+                        spaceIndex++;
+                    }
+                }
+                
+            }
+
+            Console.WriteLine("Map completed");
+            
+            //PrintIndividualsBlocks(blocks);
+
+            var defragging = true;
+            
+            while (defragging)
+            {
+                var orderedBlocks = blocks.OrderBy(c => c.Key);
+                var lastFileBlock = 
+                    orderedBlocks.Last(c => c.Value is { Type: BlockType.File, HasFragmented: false });
+                
+                var firstSpaceBlock =
+                    orderedBlocks.FirstOrDefault(c => c.Value.Type == BlockType.Space 
+                                                      && c.Value.Size >= lastFileBlock.Value.Size && !c.Value.HasFragmented);
+
+                if (firstSpaceBlock.Value == null)
+                {
+                    var fileValue = lastFileBlock.Value;
+                    fileValue.HasFragmented = true;
+                    blocks[lastFileBlock.Key] = fileValue;
+                }
+                else
+                {
+                    var freeSpace = firstSpaceBlock.Value.Size - lastFileBlock.Value.Size;
+
+                    if (freeSpace == 0)
+                    {
+                        var newFileValue = new Block
+                        {
+                            Type = lastFileBlock.Value.Type,
+                            Size = lastFileBlock.Value.Size,
+                            HasFragmented = true,
+                            Id = lastFileBlock.Value.Id
+                        };
+                        blocks[firstSpaceBlock.Key] = newFileValue;
+                        
+                        var newSpaceValue = new Block
+                        {
+                            Type = firstSpaceBlock.Value.Type,
+                            Size = firstSpaceBlock.Value.Size,
+                            HasFragmented = true,
+                            Id = firstSpaceBlock.Value.Id
+                        };
+                        blocks[lastFileBlock.Key] = newSpaceValue;
+                        
+                        // blocks[firstSpaceBlock.Key] = lastFileBlock.Value;
+                        //
+                        // var spaceValue = firstSpaceBlock.Value;
+                        //
+                        // blocks[lastFileBlock.Key] = spaceValue;
+                    }
+                    else
+                    {
+                        var remainingSpaceValue = new Block
+                        {
+                            Type = BlockType.Space,
+                            Size = freeSpace,
+                            HasFragmented = false,
+                            Id = firstSpaceBlock.Value.Id
+                        };
+                        
+                        var shiftingSpaceValue = new Block
+                        {
+                            Type = BlockType.Space,
+                            Size = lastFileBlock.Value.Size,
+                            HasFragmented = true,
+                            Id = firstSpaceBlock.Value.Id
+                        };
+                        
+                        var newFileValue = new Block
+                        {
+                            Type = lastFileBlock.Value.Type,
+                            Size = lastFileBlock.Value.Size,
+                            HasFragmented = true,
+                            Id = lastFileBlock.Value.Id
+                        };
+                        
+                        blocks[firstSpaceBlock.Key] = newFileValue;
+                        
+                        blocks[lastFileBlock.Key] = shiftingSpaceValue;
+                        
+                        blocks.Add(firstSpaceBlock.Key + 1, remainingSpaceValue);
+                    }
+                    
+                }
+                
+                //PrintIndividualsBlocks(blocks);
+                
+                var remainingFileBlocks = 
+                    blocks.Where(c => c.Value is { Type: BlockType.File, HasFragmented: false });
+                
+                Console.WriteLine($"Defragging... {DateTime.Now} Remaining: {remainingFileBlocks.Count()}" );
+
+                if (remainingFileBlocks.Count() == 0)
+                {
+                    defragging = false;
+                }
+            }
+
+            Int64 total = 0;
+            
+            int position = 0;
+            
+            var orderedB = blocks.OrderBy(c => c.Key);
+            
+            foreach (var block in orderedB)
+            {
+                if (block.Value.Type == BlockType.File)
+                {
+                    for (int blockCount = 0; blockCount < block.Value.Size; blockCount++)
+                    {
+                        Int64 sum = position * block.Value.Id;
+                        Console.WriteLine($"{position} * {block.Value.Id} = {sum}");
+                
+                        total = total + sum;
+
+                        position++;
+
+                        //Console.WriteLine("Calculating: " + total);
+                    }
+                }
+                
+                if (block.Value.Type == BlockType.Space)
+                {
+                    position = position + block.Value.Size;
+                }
+            }
+            
+            PrintIndividualsBlocks(blocks);
+            
+            Console.WriteLine("check sum: " + total);
+        }
+
+        static void PrintIndividualsBlocks(Dictionary<int, Block> blocks)
+        {
+            //string newDiskMap = "";
+
+            var orderedBlocks = blocks.OrderBy(c => c.Key);
+            
+            foreach (var block in orderedBlocks)
+            {
+                for (int blockCount = 0; blockCount < block.Value.Size; blockCount++)
+                {
+                    if (block.Value.Type == BlockType.File)
+                    {
+                        Console.Write(block.Value.Id);
+                        //newDiskMap = newDiskMap + "x" + block.Value.Id;
+                    }
+                    else
+                    {
+                        Console.Write(".");
+                        //newDiskMap = newDiskMap + ".";
+                    }
+                }
+            }
+            Console.WriteLine();
         }
     }
 
@@ -158,7 +351,9 @@ namespace Aoc2024.Day09
 
         public int Id { get; set; }
 
-        //public int Size { get; set; }
+        public int Size { get; set; }
+
+        public bool HasFragmented = false;
     }
 
     public enum BlockType
--- src/Aoc2024/Aoc2024/Program.cs
+++ src/Aoc2024/Aoc2024/Program.cs
@@ -1,3 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Aoc2024.Day09.Solution.Part01();
+Aoc2024.Day09.Solution.Part02();
