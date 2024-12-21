2863144270f3db19b876f2f1fc7bca49c13c039e
yohan-oc <129070827+yohan-oc@users.noreply.github.com>
2024-12-21 14:07:46 +0000

    day 19 part 02

--- src/Aoc2024/Aoc2024/Day19/Solution.cs
+++ src/Aoc2024/Aoc2024/Day19/Solution.cs
@@ -60,6 +60,56 @@ namespace Aoc2024.Day19
 
         public static void Part02()
         {
+            Console.WriteLine("**** Day 13 - Part 02 ****");
+
+            string[] lines = File.ReadAllLines("Day19/input.txt");
+
+            var towelPatterns = new HashSet<string>();
+
+            foreach (var pattern in lines[0].Split(","))
+            {
+                towelPatterns.Add(pattern.Trim());
+            }
+            
+            long count = 0;
+
+            int index = 1;
+            
+            foreach (var line in lines.Skip(2))
+            {
+                var cache = new Dictionary<string, long>();
+                long result = CalculateDesigns(line, towelPatterns, cache);
+                count = count + result;
+
+                Console.WriteLine($"{index} of {lines.Length}");
+                index++;
+            }
+            
+            Console.WriteLine($"Total :{count}");
+        }
+        
+        
+        static long CalculateDesigns(string design, HashSet<string> patterns, Dictionary<string, long> cache)
+        {
+            if (cache.ContainsKey(design))
+                return cache[design];
+            
+            if (string.IsNullOrEmpty(design))
+                return 1;
+
+            long count = 0;
+
+            foreach (var pattern in patterns)
+            {
+                if (design.StartsWith(pattern))
+                {
+                    var remaining = design.Substring(pattern.Length);
+                    count = count + CalculateDesigns(remaining, patterns, cache);
+                }
+            }
+
+            cache[design] = count;
+            return count;
         }
     }
 }
