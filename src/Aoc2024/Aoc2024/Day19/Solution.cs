c2ca732f100c6ec70a1f6936e708563893aa2341
yohan-oc <129070827+yohan-oc@users.noreply.github.com>
2024-12-21 02:27:12 +0000

    day 19 part 01

---
+++ src/Aoc2024/Aoc2024/Day19/Solution.cs
@@ -0,0 +1,65 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Data.Common;
+using System.Diagnostics.CodeAnalysis;
+using System.Drawing;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Aoc2024.Day19
+{
+    public static class Solution
+    {
+        public static void Part01()
+        {
+            Console.WriteLine("**** Day 13 - Part 01 ****");
+
+            string[] lines = File.ReadAllLines("Day19/input.txt");
+
+            var towelPatterns = new HashSet<string>();
+
+            foreach (var pattern in lines[0].Split(","))
+            {
+                towelPatterns.Add(pattern.Trim());
+            }
+            
+            int possibleDesigns = 0;
+            
+            foreach (var line in lines.Skip(2))
+            {
+                if (CanConstructDesign(line, towelPatterns))
+                {
+                    possibleDesigns++;
+                }
+            }
+            
+            Console.WriteLine($"Total tokens:{possibleDesigns}");
+        }
+        
+        static bool CanConstructDesign(string design, HashSet<string> patterns)
+        {
+            if (string.IsNullOrEmpty(design))
+                return true;
+
+            foreach (var pattern in patterns)
+            {
+                if (design.StartsWith(pattern))
+                {
+                    var remaining = design.Substring(pattern.Length);
+                    if (CanConstructDesign(remaining, patterns))
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        public static void Part02()
+        {
+        }
+    }
+}
